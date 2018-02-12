cc2b0341e17a13a45ca3008a984e52e90770df42
PointMotion <pointmotion@Andrews-MacBook-Pro-4.local>
2018-02-12 10:59:41 -0500

    before converting to MVC

--- Notepad_Swift/ViewController.swift
+++ Notepad_Swift/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController, UITableViewDelegate {
     var notes: [NSManagedObject] = []
     var currentIndex:Int?
     var managedContext:NSManagedObjectContext?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
